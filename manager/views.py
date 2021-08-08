3bfbd58c5defb343adc1dcd55f28f2d3428fca35
Ashwin Iyer <ashwiniyer1706@gmail.com>
2021-08-08 06:12:05 +0530

    Added simple encryption

--- manager/views.py
+++ manager/views.py
@@ -2,15 +2,42 @@ from django.http.response import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
-from .forms import PasswordModelForm
 
+
+from .forms import PasswordModelForm
+from manager.config import STANDARD_OFFSET
 from .models import PasswordModel
 # Create your views here.
 
 
+def encrypt(plainText):
+    """
+    Standard Caesar Cipher Implementation
+    """
+    cipherText = ""
+    for char in plainText:
+        cipherText += chr(ord(char)+STANDARD_OFFSET)
+    return cipherText
+
+
+def decrypt(cipherText):
+    """
+    Decrypt Caesar Cipher
+    """
+    plainText = ""
+    for char in cipherText:
+        plainText += chr(ord(char)-STANDARD_OFFSET)
+    return plainText
+
+
 @login_required(login_url="/accounts/login/")
 def showData(request):
+    """
+    View that shows user's created passwords
+    """
     data = PasswordModel.objects.filter(user=request.user)
+    for dataObject in data:
+        dataObject.passwordSaved = decrypt(dataObject.passwordSaved)
     return render(request, 'home.html', {'data': data})
 
 
@@ -21,6 +48,7 @@ def addNewPassword(request):
         if(form.is_valid()):
             instance = form.save(commit=False)
             instance.user = request.user
+            instance.passwordSaved = encrypt(instance.passwordSaved)
             instance.save()
             return HttpResponseRedirect(reverse('manager:home'))
     else:
