35b7da6058f64f40202453442d60447a79a33d75
Ashwin Iyer <ashwiniyer1706@gmail.com>
2021-08-15 08:01:42 +0530

    writing test cases

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,41 @@
+from django.urls.base import reverse
+from accounts.models import CustomUser
 from django.test import TestCase
 
 # Create your tests here.
+
+
+class AuthTests(TestCase):
+    def makeNewUser(self):
+        self.credentials = {
+            'username': 'testuser',
+            'password': 'secretpwd'
+        }
+        CustomUser.objects.create_user(**self.credentials)
+
+    def test_login(self):
+        """
+        Test login for valid user
+        """
+        self.credentials = {
+            'username': 'testuser',
+            'password': 'secretpwd'
+        }
+        CustomUser.objects.create_user(**self.credentials)
+        response = self.client.post(
+            reverse('login'), self.credentials, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(response.context['user'].is_authenticated)
+
+    def test_login_with_wrong_credentials(self):
+        """
+        Test login with invalid credentials
+        """
+        self.credentials = {
+            'username': 'testuser2',
+            'password': 'secretpw'
+        }
+        response = self.client.post(
+            reverse('login'), self.credentials, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertFalse(response.context['user'].is_authenticated)
--- manager/tests.py
+++ manager/tests.py
@@ -1,3 +1,41 @@
+from django.db.models.query import QuerySet
+from django.http import response
 from django.test import TestCase
-
+from django.urls.base import reverse
+from .models import PasswordModel
+from accounts.models import CustomUser
 # Create your tests here.
+
+
+def createCustomPassword():
+    user = CustomUser()
+    user.save()
+    return PasswordModel.objects.create(websiteLink="1234", websiteName="google.com", userName="captpicard", passwordSaved="123", user=user)
+
+
+class PasswordModelTests(TestCase):
+    def test_two_identical_objects(self):
+        """
+        Two objects with all attributes the same
+        """
+        user = CustomUser(username="Django")
+        user.save()
+        items = {
+            "websiteLink": '1234',
+            "websiteName": "google.com",
+            "userName": "captpicard",
+            "passwordSaved": "123",
+            "user": user
+        }
+        response = self.client.post(
+            reverse('manager:newPassword'), items, follow=True)
+        response2 = self.client.post(
+            reverse('manager:newPassword'), items, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response2.status_code, 200)
+        query = PasswordModel.objects.filter(websiteLink=items['websiteLink'],
+                                             userName=items['userName'],
+                                             passwordSaved=items["passwordSaved"],
+                                             user=items["user"])
+        print(query)
+        self.assertTrue(len(query) == 2)
